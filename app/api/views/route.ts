a702ab558abc15be30af47e7e65f7be74c4960b0
Shakil Ilham <ilhamshakil@gmail.com>
2023-10-21 06:48:11 +0530

    commit-18

--- app/api/views/route.ts
+++ app/api/views/route.ts
@@ -11,7 +11,7 @@ export async function GET(request: NextRequest) {
                 viewId: viewId,
             }
         })
-        return NextResponse.json(view);
+        return NextResponse.json({status : "done"});
     }
-    return null;
+    return NextResponse.json({status : "error"});
 }
